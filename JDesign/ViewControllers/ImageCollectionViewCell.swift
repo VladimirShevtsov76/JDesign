0d76913c0dcabe617c989eae69e4e7785f8fdbc4
Vladimir Shevtsov <work2976@gmail.com>
2021-10-25 21:29:55 +0300

    Version 0.4

--- JDesign/ViewControllers/ImageCollectionViewCell.swift
+++ JDesign/ViewControllers/ImageCollectionViewCell.swift
@@ -4,15 +4,28 @@
 //
 //  Created by Vladimir Shevtsov on 23.10.2021.
 //
-
+import Foundation
 import UIKit
+
+//protocol CurrentCellTypeDelegate: AnyObject {
+//    
+//    func setMainImage(number: Int)
+//}
 
 class ImageCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var photoView: UIImageView!
+    
+    //weak var delegate: CurrentCellTypeDelegate?
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    override var isSelected: Bool {
+        didSet {
+            backgroundColor = isSelected ? .black : .clear
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "myNoteKey"), object: nil, userInfo: ["text": self.tag])
+        }
+    }
 }
--- JDesign/ViewControllers/ViewController.swift
+++ JDesign/ViewControllers/ViewController.swift
@@ -4,8 +4,10 @@
 //
 //  Created by Vladimir Shevtsov on 16.10.2021.
 //
-
+import Foundation
 import UIKit
+
+let myNoteKey = "com.andrewcbancroft.specialNotificationKey"
 
 class ViewController: UIViewController {
     
@@ -14,23 +16,24 @@ class ViewController: UIViewController {
     var imagesType = [UIImage]()
     
     @IBOutlet var mainView: UIView!
-    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var mainImage: UIImageView!
     
     @IBOutlet weak var kitView: UICollectionView!
-    @IBOutlet weak var typeView: UICollectionView!
     @IBOutlet weak var colorView: UICollectionView!
-        
+    @IBOutlet weak var typeView: UICollectionView!
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        
         setGestures()
         
         typeView.dataSource = self
         typeView.delegate   = self
-        kitView.dataSource = self
-        kitView.delegate   = self
         
-        let fullArray = getSourceStringArray()
+        NotificationCenter.default.addObserver(self, selector: #selector(self.incomingNotification(_:)), name:  NSNotification.Name(rawValue: "myNoteKey"), object: nil)
+        
+        //let fullArray = getSourceStringArray()
         
         for i in 0...22 {
             let image = UIImage(named: "\(i+1)")!
@@ -38,6 +41,13 @@ class ViewController: UIViewController {
         }
         
     }
+    
+    @objc func incomingNotification(_ notification: Notification) {
+        if let text = notification.userInfo?["text"] as? Int {
+            mainImage.image = imagesType[text-1]            // do something with your text
+        }
+    }
+    
     
     fileprivate func setGestures() {
         let swipeRightGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleGestureR(gesture:)))
@@ -48,38 +58,38 @@ class ViewController: UIViewController {
         swipeLeftGesture.direction  = UISwipeGestureRecognizer.Direction.left
         swipeUpGesture.direction    = UISwipeGestureRecognizer.Direction.up
         swipeDownGesture.direction  = UISwipeGestureRecognizer.Direction.down
-        img.addGestureRecognizer(swipeRightGesture)
-        img.addGestureRecognizer(swipeLeftGesture)
-        img.addGestureRecognizer(swipeUpGesture)
-        img.addGestureRecognizer(swipeDownGesture)
+        mainImage.addGestureRecognizer(swipeRightGesture)
+        mainImage.addGestureRecognizer(swipeLeftGesture)
+        mainImage.addGestureRecognizer(swipeUpGesture)
+        mainImage.addGestureRecognizer(swipeDownGesture)
     }
     
     // GestureRecognizer
     @objc func handleGestureR(gesture: UISwipeGestureRecognizer) -> Void {
         imageColor = imageColor - 1
         let image = "img_\(imageNumber)_\(imageColor)"
-        img.image = UIImage(named: image)
+        mainImage.image = UIImage(named: image)
     }
     
     // GestureRecognizer
     @objc func handleGestureL(gesture: UISwipeGestureRecognizer) -> Void {
         imageColor = imageColor + 1
         let image = "img_\(imageNumber)_\(imageColor)"
-        img.image = UIImage(named: image)
+        mainImage.image = UIImage(named: image)
     }
     
     // GestureRecognizer
     @objc func handleGestureUp(gesture: UISwipeGestureRecognizer) -> Void {
         imageNumber = imageNumber + 1
         let image = "img_\(imageNumber)_\(imageColor)"
-        img.image = UIImage(named: image)
+        mainImage.image = UIImage(named: image)
     }
     
     // GestureRecognizer
     @objc func handleGestureDown(gesture: UISwipeGestureRecognizer) -> Void {
         imageNumber = imageNumber - 1
         let image = "img_\(imageNumber)_\(imageColor)"
-        img.image = UIImage(named: image)
+        mainImage.image = UIImage(named: image)
     }
 }
 
@@ -91,7 +101,9 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellImage", for: indexPath) as! ImageCollectionViewCell
         let image = imagesType[indexPath.item]
+        cell.tag  = indexPath.item + 1 //name of image file
         cell.photoView.image = image
         return cell
     }
 }
+
